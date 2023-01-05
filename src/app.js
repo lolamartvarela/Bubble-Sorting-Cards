70e4ea29f7195c574e11e58f0c3a7caeb29b9124
lolamartvarela <luciamvarela@outlook.com>
2023-01-05 15:25:21 +0000

    Avance

--- src/app.js
+++ src/app.js
@@ -24,31 +24,74 @@ window.onload = function() {
     "J",
     "As"
   ];
-  let indexPalos = Math.floor(Math.random() * palos.length);
-  let indexNumeros = Math.floor(Math.random() * numeros.length);
 
-  let color =
-    palos[indexPalos] == "♦" || palos[indexPalos] == "♥" ? "red" : "black";
+  /*let color =
+                              palos[indexPalos] == "♦" || palos[indexPalos] == "♥" ? "red" : "black";
 
-  console.log(palos[indexPalos]);
-  let top = document.getElementById("top");
-  let bottom = document.getElementById("bottom");
-  let number = document.getElementById("number");
-  top.innerHTML = palos[indexPalos];
+                            console.log(palos[indexPalos]);
+                            let top = document.getElementById("top");
+                            let bottom = document.getElementById("bottom");
+                            let number = document.getElementById("number");
+                            top.innerHTML = palos[indexPalos];
 
-  document.getElementById("top").style.color = color;
-  document.getElementById("bottom").style.color = color;
-  document.getElementById("number").style.color = color;
-  bottom.innerHTML = palos[indexPalos];
-  number.innerHTML = numeros[indexNumeros];
+                            document.getElementById("top").style.color = color;
+                            document.getElementById("bottom").style.color = color;
+                            document.getElementById("number").style.color = color;
+                            bottom.innerHTML = palos[indexPalos];
+                            number.innerHTML = numeros[indexNumeros];*/
 
-  for (let i = 1; i <= 10; i++) {
-    document.body.innerHTML += `<div class="card" style="width: 18rem;">
+  //declaramos un array que tendra todas las cartas
+  let todasCartas = [];
+
+  for (let i = 1; i <= 20; i++) {
+    let indexPalos = Math.floor(Math.random() * palos.length);
+    let indexNumeros = Math.floor(Math.random() * numeros.length);
+
+    //aca obtengo el palo
+    console.log(palos[indexPalos]);
+    //aca obtengo el numero
+    console.log(numeros[indexNumeros]);
+    //generamos la variable carta que es el array que tendra el conjunto de datos; palos y numeros
+    let carta = [palos[indexPalos], numeros[indexNumeros]];
+    console.log(carta);
+    //llamamos al array y con push ingresamos cada conjunto de datos que se genera con iteracion
+    todasCartas.push(carta);
+    console.log(todasCartas);
+
+    if (palos[indexPalos] === "♥" || palos[indexPalos] === "♦") {
+      document.body.innerHTML += `<div class="card" style="width: 18rem;">
     <ul class="list-group list-group-flush">
       <li class="list-group-item" id="top">${palos[indexPalos]}</li>
       <li class="list-group-item" id="number">${numeros[indexNumeros]}</li>
       <li class="list-group-item" id="bottom">${palos[indexPalos]}</li>
     </ul>
   </div>`;
+    } else {
+      document.body.innerHTML += `<div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item" id="top">${palos[indexPalos]}</li>
+    <li class="list-group-item" id="number">${numeros[indexNumeros]}</li>
+    <li class="list-group-item" id="bottom">${palos[indexPalos]}</li>
+  </ul>
+</div>`;
+    }
   }
+  const bubbleSort = todasCartas => {
+    let wall = todasCartas.length - 1; //iniciamos el wall o muro al final del array
+    while (wall > 0) {
+      let index = 0;
+      while (index < wall) {
+        //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
+        if (todasCartas[index] > todasCartas[index + 1]) {
+          let aux = todasCartas[index];
+          todasCartas[index] = todasCartas[index + 1];
+          todasCartas[index + 1] = aux;
+        }
+        index++;
+      }
+      wall--; //disminuir la pared para optimizar
+    }
+    console.log(bubbleSort);
+    return arr;
+  };
 };
